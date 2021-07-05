5e8a18a8ecef28143caa9c44f8010eb7e12e1c42
tylerchism <tchism4@gmail.com>
2021-07-04 17:33:19 -0700

    added script for first neuron class

---
+++ neuron_object.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+X = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+Layer1 = Layer_Dense(4, 5)
+Layer2 = Layer_Dense(5, 2)
+
+Layer1.forward(X)
+Layer2.forward(Layer1.output)
+print(Layer2.output)
